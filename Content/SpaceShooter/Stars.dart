33069230537e20ec0e84bd18e7b0421be3b5e765
Eric J. Smith <eric@codesmithtools.com>
2012-08-03 13:26:23 -0500

    Revert "Some changes."
    This reverts commit 1ccfde2b2489fa65e7e3a312d5aec618f65ccb4b.

--- Content/SpaceShooter/Stars.dart
+++ Content/SpaceShooter/Stars.dart
@@ -1,12 +1,18 @@
 class Stars extends GameEntity {
+  num xVel;
+  
   Stars(Game game, num x, num y, num h, num w) : super.withPosition(game, x, y, h, w) {
     opacity = Utils.random(.5, 1);
     color = "255, 255, 255";
-    momentum.xVel = Utils.random(-20, -50);
+    xVel = Utils.random(.5, 1.5);
   }
   
   void update() {
-    if (x < -game.halfSurfaceWidth)
+    SpaceGame g = game;
+    
+    x -= xVel;
+    
+    if (x < -g.halfSurfaceWidth)
       removeFromGame();
     
     super.update();
--- Server/StaticFileHandler.dart
+++ Server/StaticFileHandler.dart
@@ -14,25 +14,15 @@ class StaticFileHandler {
       String mimeType = "text/html; charset=UTF-8";
       int lastDot = fileName.lastIndexOf(".", fileName.length);
       if (lastDot != -1) {
-        String extension = fileName.substring(lastDot).toLowerCase();
-        if (extension == ".css")
-          mimeType = "text/css";
-        else if (extension == ".js")
-          mimeType = "application/javascript";
-        else if (extension == ".dart")
-          mimeType = "application/dart";
-        else if (extension == ".ico")
-          mimeType = "image/vnd.microsoft.icon";
-        else if (extension == ".png")
-          mimeType = "image/png";
-        else if (extension == ".gif")
-          mimeType = "image/gif";
-        else if (extension == ".mp3")
-          mimeType = "audio/mpeg";
-        else if (extension == ".ogg")
-          mimeType = "audio/ogg";
-        else if (extension == ".ttf")
-          mimeType = "application/x-font-ttf";
+        String extension = fileName.substring(lastDot);
+        if (extension == ".css") { mimeType = "text/css"; }
+        if (extension == ".js") { mimeType = "application/javascript"; }
+        if (extension == ".dart") { mimeType = "application/dart"; }
+        if (extension == ".ico") { mimeType = "image/vnd.microsoft.icon"; }
+        if (extension == ".png") { mimeType = "image/png"; }
+        if (extension == ".gif") { mimeType = "image/gif"; }
+        if (extension == ".mp3") { mimeType = "audio/mpeg"; }
+        if (extension == ".ogg") { mimeType = "audio/ogg"; }
       }
       res.headers.set("Content-Type", mimeType);
       // Get the length of the file for setting the Content-Length header.
